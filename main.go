f114ea84e3cd9ee970c2dac1881d2d5de8641387
Andrej Jovanovic <andrej_jovanovic@epam.com>
2019-07-30 16:00:40 +0200

    Added getEvent/{id}

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-    "fmt"
-    "log"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 type Article struct {
-    Title string `json:"Title"`
-    Desc string `json:"desc"`
-    Content string `json:"content"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
 type Event struct {
-	Id string `json:"id"`
-	Title string `json:"title"`
-	Place string `json:"place"`
-	Speaker string `json:"speaker"`
+	Id        string `json:"id"`
+	Title     string `json:"title"`
+	Place     string `json:"place"`
+	Speaker   string `json:"speaker"`
 	EventType string `json:"eventType"`
-	DateTime string `json:"dateTime"`
+	DateTime  string `json:"dateTime"`
 }
 
 // let's declare a global Articles array
@@ -28,41 +29,56 @@ type Event struct {
 var Articles []Article
 var EventList []Event
 
-
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	// add our articles route and map it to our 
-    // returnAllArticles function like so
-	http.HandleFunc("/articles", returnAllArticles)
-	http.HandleFunc("/getAllEvents", returnAllEvents)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	// creates a new instance of a mux router
+	muxRouter := mux.NewRouter().StrictSlash(true)
+	muxRouter.HandleFunc("/", homePage)
+	// add our articles route and map it to our
+	// returnAllArticles function like so
+	muxRouter.HandleFunc("/articles", returnAllArticles)
+	muxRouter.HandleFunc("/getAllEvents", returnAllEvents)
+	muxRouter.HandleFunc("/getEvent/{Id}", returnSingleEvent)
+	log.Fatal(http.ListenAndServe(":10000", muxRouter))
 }
 
-func returnAllArticles(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(Articles)
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
 }
 
-func returnAllEvents(w http.ResponseWriter, r *http.Request){
-    fmt.Println("Endpoint Hit: returnAllArticles")
-    json.NewEncoder(w).Encode(EventList)
+func returnAllEvents(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllEvents")
+	json.NewEncoder(w).Encode(EventList)
 }
 
+func returnSingleEvent(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["Id"]
+
+	//fmt.Fprintf(w, "Key: " + key)
+	for _, events := range EventList {
+		if events.Id == key {
+			json.NewEncoder(w).Encode(events)
+		}
+	}
+}
 
 func main() {
-    Articles = []Article{
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
 		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-        Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
-	
-	EventList = []Event{
-		Event{Id: "1", Title: "Hello World", Place: "Malaga", Speaker: "Andrej", EventType: "Webinar", DateTime: "24.9.2018"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 
-    handleRequests()
+	EventList = []Event{
+		Event{Id: "1", Title: "Hello World", Place: "Malaga", Speaker: "Andrej", EventType: "Webinar", DateTime: "24.9.2018"},
+		Event{Id: "2", Title: "Hello Death", Place: "Hell", Speaker: "Trump", EventType: "Live", DateTime: "6.6.1489"},
+	}
+
+	handleRequests()
 }
